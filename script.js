7c6d270ccd40fc282e0493d297b19fde5eb08c3a
huanillo <102235967+huanillo@users.noreply.github.com>
2025-03-15 17:25:49 +0100

    Created basic structure of html, css and first attempt at .js Next step, create the button to askUser the number of square and play with it on the .js

---
+++ script.js
@@ -0,0 +1,9 @@
+function createSquare(){
+let num = askUser;
+for (i < num; i++; i = num){
+    // create a new div element
+    const div = document.createElement("div");
+    div.className = "square";
+    document.grid.appendChild(div);
+    }
+}
